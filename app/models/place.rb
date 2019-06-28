c6aebce05a859e7a0bfdc3c059e85ecf3f471762
danniedanniedannie <danielle.simone.lynch@gmail.com>
2019-06-28 13:51:05 +0000

    troubleshooting Google maps API

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,14 +1,14 @@
 class Place < ApplicationRecord
 	 belongs_to :user
-	 has_many :comments, dependent: :destroy 
-	 has_many :photos, dependent: :destroy
+	 has_many :comments 
+     has_many :photos 
 
-  
-  geocoded_by :address
+ geocoded_by :address
   after_validation :geocode
 
 	  validates :name, presence: true, length: {minimum: 3}
 	  validates :address, presence: true
 	  validates :description, presence: true
+
 end
 
